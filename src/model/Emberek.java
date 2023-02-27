366ee7798213c06d5eaacc8d6c650c6e6f88384b
Ódor Norbert <odor.norbert@dk.akkszalezi.hu>
2023-02-27 11:16:51 +0100

    fájlok feldolgozása sikeres

--- src/model/Emberek.java
+++ src/model/Emberek.java
@@ -3,13 +3,33 @@ package model;
 class Emberek {
     String nev;
     int kor;
-    char neme;
-    int MunkTöltÉv;
+    String neme;
+    int munkTöltÉv;
 
-    public Emberek(String nev, int kor, char neme, int MunkTöltÉv) {
+    public Emberek( String nev, int kor, String neme, int munkTöltÉv) {
         this.nev = nev;
         this.kor = kor;
         this.neme = neme;
-        this.MunkTöltÉv = MunkTöltÉv;
+        this.munkTöltÉv = munkTöltÉv;
     }
+
+    public String getNev() {
+        return nev;
+    }
+
+    public int getKor() {
+        return kor;
+    }
+
+    public String getNeme() {
+        return neme;
+    }
+
+    public int getMunkTöltÉv() {
+        return munkTöltÉv;
+    }
+    
+    
+    
+
 }
--- src/model/FajlBeolvas.java
+++ src/model/FajlBeolvas.java
@@ -1,23 +1,34 @@
 package model;
 
-import java.awt.List;
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Scanner;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import view.DolgozokGui;
 
 public class FajlBeolvas {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         beolvas();
     }
 
-    public static void beolvas() {
-      String fajlnev = "emberek.txt";
-       List<String> sorok = Files.readAllLines(Path.of(fajlnev));
-       sorok.remove(0);
-       return sorok;
-              
+    public static ArrayList<Emberek> beolvas() throws IOException {
+        Path path = Paths.get("emberek.txt");
+        java.util.List<String> sorok = Files.readAllLines(path);
+        sorok.remove(0);
+        ArrayList<Emberek> dolgozok = new ArrayList<>();
+
+        for (int i = 0; i < sorok.size(); i++) {
+            String[] egySor = sorok.get(i).split(";");
+                if (egySor.length<= 3) {
+               dolgozok.add(new Emberek(egySor[0], Integer.parseInt(egySor[1]), egySor[2], 0));
+                } else {
+               dolgozok.add(new Emberek(egySor[0], Integer.parseInt(egySor[1]), egySor[2], Integer.parseInt(egySor[3])));
+                }
+            }
+    return dolgozok;
+        }
+
     }
-}
+
